0673ef8ff4d3785cee8e64ccc67ac6b47b804945
bayansaffour <126967856+bayansaffour@users.noreply.github.com>
2025-07-16 19:04:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -40,9 +40,7 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 // Middleware
-app.use(cors({
-  origin: 'https://daralhussam.onrender.com'  // استبدل بالرابط الصحيح لواجهة المستخدم عندك
-}));
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
